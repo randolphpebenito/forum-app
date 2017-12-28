e852806fc0ec9a431a8513ea0c960af7c7a999cf
Randolph Pe Benito <rpebenito@xyber.ph>
2017-12-28 22:21:25 +0800

    Add boards app layout, templates and test.

--- webapp/boards/views.py
+++ webapp/boards/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Board
+
+def home(request):
+     boards = Board.objects.all()
+     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
